14b254757eed2024e2470e3e2ce9b214b1338dd2
Nahom Mehanzel <nknahom@gmail.com>
2021-08-07 01:32:56 +0000

    addOffspring setup

--- vampire.js
+++ vampire.js
@@ -10,23 +10,20 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
-  get numberOfVampiresFromOriginal() {
-
-  }
+  get numberOfVampiresFromOriginal() {}
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
-
-  }
+  isMoreSeniorThan(vampire) {}
 
   /** Stretch **/
 
@@ -35,10 +32,7 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
-  }
+  closestCommonAncestor(vampire) {}
 }
 
 module.exports = Vampire;
-
